5ed4cca511b2559d38f5d76696df6a63b87a6b97
fnixsik <fnixsikMen@mail.ru>
2025-09-18 23:32:23 +0500

    Авторизация Step_2

---
+++ demo/src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.demo.security;
+
+import com.example.demo.repository.roles.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import com.example.demo.entity.roles.User;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService  {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles().stream()
+                        .map(role -> "ROLE_" + role.getName())
+                        .toArray(String[]::new))
+                .build();
+
+    }
+}
--- demo/src/main/java/com/example/demo/security/JwtService.java
+++ demo/src/main/java/com/example/demo/security/JwtService.java
@@ -1,4 +1,61 @@
 package com.example.demo.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.*;
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "my-first-secret-key-for-dar";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    private final Key key;
+
+    public JwtService(){
+        this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    // Генерация токена
+    public String generateToken(String username, Set<String> roles){
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Извлечение username
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    // Извлечение ролей
+    @SuppressWarnings("unchecked")
+    public Set<String> extractRoles(String token) {
+        return new HashSet<>(
+                (List<String>) extractAllClaims(token).get("roles")
+        );
+    }
+
+    // Проверка валидности
+    public boolean isTokenValid(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
